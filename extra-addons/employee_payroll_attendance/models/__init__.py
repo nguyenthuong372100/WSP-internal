025f22bd2e51f65a7f444f574a27cc307c7071c1
Hoàng Duy Lập <115389385+DuyLap23@users.noreply.github.com>
2025-01-06 10:57:58 +0700

    Delete extra-addons directory

--- extra-addons/employee_payroll_attendance/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import hr_attendance_payroll
-from . import hr_payslip_report
-from . import payslip_attendance
-from . import generate_salary_wizard
-from . import account_analytic_line
-from . import hr_payslip
