06f05a8e3eddb0f0574b7daf622a4057aebfe849
DuyLap <hoangduylap124@gmail.com>
2025-01-06 11:18:27 +0700

    revert extradons

---
+++ extra-addons/custom_sale_timesheet/__init__.py
@@ -0,0 +1,2 @@
+# test
+from . import models
---
+++ extra-addons/employee_payroll_attendance/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Employee Payroll Based on Attendance Approval',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'Calculate payroll based on approved attendance records',
+    'description': 'This module integrates attendance approval into payroll calculation in Odoo.',
+    'author': 'Your Name',
+    'depends': ['hr_attendance','sale_timesheet','web','base', 'hr_timesheet', 'hr_timesheet'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_attendance_payroll_views.xml',
+        'views/hr_payslip_views.xml',
+        'views/hr_employee_payslip_views.xml',
+        'views/generate_salary_wizard_views.xml',
+        'views/account_analytic_line_views.xml',
+        'views/custom_report_layout.xml',
+        'views/hr_payslip_views_update.xml',
+        'views/menu_reporting.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
